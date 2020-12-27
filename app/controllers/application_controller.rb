402c139ae15fbcfeadec09e8d3797a783d17440d
fentyhall <fentymhall@gmail.com>
2020-12-26 20:04:35 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user 
+    session[:user_id]
+  end 
+
+  def logged_in?
+
+  end 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController 
     def new
+        @user = User.new
     end 
 
     def create 
-        session[:user_id] = @user.id
+        if @user = User.find_by(name: params[:user][:name])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else  
+            render :new 
+        end 
     end 
 end 

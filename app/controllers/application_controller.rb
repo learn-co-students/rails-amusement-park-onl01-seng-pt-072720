c16a9d65b20fb09c7eec9686eb77e6631495cc6e
sergiocardenasg <sergiocardenas@utexas.edu>
2021-01-09 12:19:09 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def home 
+  end 
+
+  def current_user 
+    @user = User.find_by(id: session[:user_id])
+  end 
+
+  def logged_in?
+    redirect_to '/' unless current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    validates :password_digest, presence: true
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        if nausea > happiness
+            'sad'
+        else
+            'happy'
+        end
+    end
 end

648fab7d4c721c917aa055593ef3474833fa2b3e
Shasel <github email address>
2021-01-09 20:39:13 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user          
+        end
+        
+    end
+
+    private
+ 
+    def user_params
+      params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+
+  resources :users
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  # get '/users/new' => 'users#new'
+  post '/logout' => 'sessions#destroy'
+  
+  # post '/take_ride' => 'users#ride'
+
+
 end

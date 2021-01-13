72688c468b7914b3678b619a246336e64f679451
danainjax <danainjax@gmail.com>
2021-01-13 15:34:41 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
-end
+  has_secure_password
+    validates :password_digest, presence: true
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        unless admin
+            if nausea > happiness
+                'sad'
+            else
+                'happy'
+            end
+        end
+    end
+end	
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users
+  resources :attractions
+  resources :rides
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
